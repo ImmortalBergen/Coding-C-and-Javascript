686a0c3a55c7a072724835b6b19c6646b6313722
ImmortalBergen <76254788+ImmortalBergen@users.noreply.github.com>
2021-03-28 11:52:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+using System.Collections;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"C:\Users\Maxenbergen\Desktop\ts\programm\code\i\cracker.py";
+            string[] test = {"3"};
+            while (true)
+            {
+                Console.WriteLine("Starting or reapiting");
+                string text = System.IO.File.ReadAllText(@"C:\Users\Maxenbergen\Desktop\Websocket coding\Commands.txt");
+                int number = Int32.Parse(text);
+                if (number == 1)
+                {
+                    Console.WriteLine("Das Licht wird eingeschalten");
+                    
+
+
+                }
+                if (number == 0)
+                {
+                    Console.WriteLine("Das Licht wird ausgeschalten");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+                System.IO.File.WriteAllLines(@"C:\Users\Maxenbergen\Desktop\Websocket coding\Commands.txt", test);
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
